dc237ff5760fa307b984c30789eadbe6d3e8a763
impact-eintr <yixingwei4@gmail.com>
2021-07-14 17:21:09 +0800

    0.75

--- .ccls-cache/@home@eintr@Applications@st/config.h
+++ .ccls-cache/@home@eintr@Applications@st/config.h
@@ -95,7 +95,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.66;
+float alpha = 0.75;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
